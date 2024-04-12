e8c36b64fd6178606ac7e38d587e4203603ab85e
Amarilla Kevin <amarelak15@gmail.com>
2024-04-11 22:54:23 -0300

    Agrego Docstring a las funciones

--- modulo/__init__.py
+++ modulo/__init__.py
@@ -1,4 +1,17 @@
 def constructor(names, goals, goals_avoided, assists):
+    """
+    Construye un diccionario a partir de los nombres de los jugadores y sus estadísticas de rendimiento.
+
+    Parameters:
+    names (str): Una cadena que contiene los nombres de los jugadores separados por comas.
+    goals (list): Una lista que contiene la cantidad de goles anotados por cada jugador.
+    goals_avoided (list): Una lista que contiene la cantidad de goles evitados por cada jugador.
+    assists (list): Una lista que contiene la cantidad de asistencias realizadas por cada jugador.
+
+    Returns:
+    dict: Un diccionario que mapea el nombre de cada jugador a una tupla de sus estadísticas de rendimiento, donde el primer elemento de la tupla representa la cantidad de goles, 
+    el segundo elemento representa la cantidad de goles evitados y el tercer elemento representa la cantidad de asistencias.
+    """
     estadisticas = tuple(zip(goals, goals_avoided, assists))
     nombres = names.split(", ")
     diccionario = dict(zip(nombres, estadisticas))
--- modulo/cuentas.py
+++ modulo/cuentas.py
@@ -1,8 +1,26 @@
 def goleador (diccionario):
+    """
+    Devuelve el jugador/a con la mayor cantidad de goles.
+
+    Parameters:
+    diccionario (dict): Un diccionario que mapea el nombre del jugador/a a una tupla de estadísticas de rendimiento, donde el primer elemento de la tupla representa la cantidad de goles.
+
+    Returns:
+    tuple: Una tupla que contiene el nombre del jugador/a y su cantidad de goles.
+    """
     max_goles= max(diccionario.items(), key = lambda x: x[1][0])
     return max_goles
 
 def mas_influyente(diccionario):
+    """
+    Devuelve el jugador/a más influyente en función de una ponderación específica de sus estadísticas.
+    
+    Parameters:
+    diccionario (dict): Un diccionario que mapea el nombre del jugador/a a una tupla de estadísticas de rendimiento.
+
+    Returns:
+    tuple: Una tupla que contiene el nombre del jugador/a más influyente y su puntaje total ponderado.
+    """
     def ponderado (items):
         return items[0] * 1.5 + items[1] * 1.25 + items[2]
     max_influyente = max(diccionario.items(), key=lambda x: ponderado(x[1]))
@@ -13,8 +31,26 @@ def mas_influyente(diccionario):
 #    suma = sum(valor[0] for valor in diccionario.values())
 #    return suma / 25
 def promedio_equipo(goles):
+    """
+    Calcula el promedio de goles por partido para todo el equipo.
+
+    Parameters:
+    goles (list): Una lista que contiene la cantidad de goles anotados por el equipo en cada uno de los 25 partidos.
+
+    Returns:
+    float: El promedio de goles por partido para todo el equipo.
+    """
     return sum(goles) / 25
 
 def promedio_goleador(top_scorer):
+    """
+    Calcula el promedio de goles por partido para el jugador/a con más goles.
+
+    Parameters:
+    top_scorer (tuple): Una tupla que contiene el nombre del jugador/a y su tupla de estadísticas de rendimiento, donde el primer elemento de la tupla representa la cantidad de goles.
+
+    Returns:
+    float: El promedio de goles por partido para el jugador/a con más goles.
+    """
     return top_scorer[1][0] / 25
     
