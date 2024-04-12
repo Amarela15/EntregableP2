0a8a9d5dc95beb0d662ef9b635ad17fe1afa1faa
Amarilla Kevin <amarelak15@gmail.com>
2024-04-11 22:32:10 -0300

    Inciso 4: Promedio Equipo

--- modulo/cuentas.py
+++ modulo/cuentas.py
@@ -8,3 +8,9 @@ def mas_influyente(diccionario):
     max_influyente = max(diccionario.items(), key=lambda x: ponderado(x[1]))
     total = ponderado(max_influyente[1])
     return max_influyente, total
+
+#def promedio_equipo(diccionario):
+#    suma = sum(valor[0] for valor in diccionario.values())
+#    return suma / 25
+def promedio_equipo(goles):
+    return sum(goles) / 25
